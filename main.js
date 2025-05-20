1f0feef6a07299bd28702141938f484486092a49
suellen-sususapeca <133366118+suellen-sususapeca@users.noreply.github.com>
2025-05-20 11:38:59 -0300

    20.05

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const botoes = document.querySelectorAll (".botao");
+const Textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i <botoes.length; i++){
     botoes [i].onclick = funcion (){
+
         for(let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
             botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
-    } 
+        Textos[i].classList.add("ativo");
+    };
 } 
+
+const computadores = document. querySelector ("contador");
+const tempoObjetivo = new Date ("2025-05-06t00:00");
